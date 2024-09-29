62cd7823f92af852fe25568bb9172f85d71b9fcf
Caio Delgado <39803009+caiodelgadonew@users.noreply.github.com>
2024-09-30 00:29:43 +0200

    add nvim-colorizer

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -227,6 +227,7 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
 	-- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
 	"tpope/vim-sleuth", -- Detect tabstop and shiftwidth automatically
+	"norcalli/nvim-colorizer.lua",
 
 	-- NOTE: Plugins can also be added by using a table,
 	-- with the first argument being the link and the following
